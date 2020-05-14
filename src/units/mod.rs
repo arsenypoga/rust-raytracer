12a914b13eec426fd32d15bd3f237301b5104ba9
arsenypoga <semiretoja@gmail.com>
2020-05-14 04:58:24 -0400

    Added tests and code for Point, Vector and Tuple

--- src/units/mod.rs
+++ src/units/mod.rs
@@ -1,1 +1,14 @@
+pub mod color;
+pub mod intersect;
+pub mod matrix;
+pub mod ray;
+pub mod sphere;
 pub mod tuple;
+pub mod utils;
+
+pub use intersect::Intersection;
+pub use matrix::{Matrix, IDENTITY_MATRIX};
+pub use ray::Ray;
+pub use sphere::Sphere;
+
+// pub use
