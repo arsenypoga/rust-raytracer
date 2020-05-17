a35e12fd68c99f24232fb00a07eedff647caadbf
illyalaifu <illyalaifu@gmail.com>
2020-05-17 04:05:56 -0400

    Added Computations, methods or simpler color computing in the World

--- src/units/intersect.rs
+++ src/units/intersect.rs
@@ -1,8 +1,11 @@
 //! Intersection operations
+use crate::units::tuple::{Point, Tuple, Vector};
 use crate::units::utils;
+use crate::units::Ray;
 use crate::units::Sphere;
 use std::cmp::Ordering;
 use std::ops;
+
 /// Intersection
 #[derive(Debug, Clone, Copy)]
 pub struct Intersection<'a> {
@@ -10,6 +13,20 @@ pub struct Intersection<'a> {
     pub t: f64,
     /// Intersected object
     pub object: &'a Sphere,
+}
+
+pub struct Computations<'a> {
+    /// T of intersection for the ray
+    pub t: f64,
+    /// Object of computations
+    pub object: &'a Sphere,
+    /// Point of intersections
+    pub point: Point,
+    /// Eye vector
+    pub eyev: Vector,
+    /// Normal vector at the point
+    pub normalv: Vector,
+    pub inside: bool,
 }
 
 impl<'a> Intersection<'a> {
@@ -20,19 +37,49 @@ impl<'a> Intersection<'a> {
 
     /// Returns hits from given intersection vector
     pub fn hit(xs: Vec<Intersection>) -> Option<Intersection> {
-        let i1 = xs[0];
-        let i2 = xs[1];
-        // let is_i1_positive =
-        if (((i1.t > 0.) && (i2.t > 0.)) && (i1.t < i2.t)) || ((i1.t > 0.) && (i2.t < 0.)) {
-            Some(i1)
-        } else if (((i1.t > 0.) && (i2.t > 0.)) && (i1.t > i2.t)) || ((i1.t < 0.) && (i2.t > 0.)) {
-            Some(i2)
-        } else {
+        if xs.len() == 0 {
             None
+        } else if xs.len() == 1 {
+            return Some(xs[0]);
+        } else {
+            let i1 = xs[0];
+            let i2 = xs[1];
+            // let is_i1_positive =
+            if (((i1.t > 0.) && (i2.t > 0.)) && (i1.t < i2.t)) || ((i1.t > 0.) && (i2.t < 0.)) {
+                Some(i1)
+            } else if (((i1.t > 0.) && (i2.t > 0.)) && (i1.t > i2.t))
+                || ((i1.t < 0.) && (i2.t > 0.))
+            {
+                Some(i2)
+            } else {
+                None
+            }
+        }
+    }
+
+    pub fn computations(&self, r: Ray) -> Computations {
+        let position = r.position(self.t);
+        let mut normalv = self.object.normal(position);
+        let eyev = -r.direction;
+        let mut inside = false;
+
+        if normalv.dot(eyev) < 0. {
+            normalv = -normalv;
+            inside = true;
+        }
+        Computations {
+            t: self.t,
+            object: self.object,
+            point: position,
+            eyev: -r.direction,
+            normalv,
+            inside,
         }
     }
 }
+
 impl<'a> Eq for Intersection<'a> {}
+
 impl<'a> Ord for Intersection<'a> {
     fn cmp(&self, other: &Self) -> Ordering {
         if (self.t - other.t) > utils::EPSILON {
@@ -66,7 +113,6 @@ impl<'a> PartialOrd for Intersection<'a> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn hit() {
         // The hit, when all intersections have positive t
@@ -94,5 +140,32 @@ mod tests {
         let xs = vec![i1, i2];
 
         assert!(Intersection::hit(xs).is_none());
+    }
+
+    #[test]
+    fn computations() {
+        // The hit, when an intersection occurs on the outside
+        let r = Ray::new(Point::new(0, 0, -5), Vector::new(0, 0, 1));
+        let shape = Sphere::default();
+        let i = Intersection::new(4., &shape);
+        let comps = i.computations(r);
+        assert_eq!(comps.t, i.t);
+        assert_eq!(comps.object, i.object);
+        assert_eq!(comps.point, Point::new(0, 0, -1));
+        assert_eq!(comps.eyev, Vector::new(0, 0, -1));
+        assert_eq!(comps.normalv, Vector::new(0, 0, -1));
+        assert!(!comps.inside);
+
+        // The hit, when an intersection occurs on the inside
+        let r = Ray::new(Point::new(0, 0, 0), Vector::new(0, 0, 1));
+        let shape = Sphere::default();
+        let i = Intersection::new(1., &shape);
+        let comps = i.computations(r);
+        assert_eq!(comps.t, i.t);
+        assert_eq!(comps.object, i.object);
+        assert_eq!(comps.point, Point::new(0, 0, 1));
+        assert_eq!(comps.eyev, Vector::new(0, 0, -1));
+        assert_eq!(comps.normalv, Vector::new(0, 0, -1));
+        assert!(comps.inside);
     }
 }
